421a9b33e72dc283d8ce641b316993a96df9ffe0
Konstantin <drpdrp@mail.ru>
2021-09-03 23:20:18 +0300

    CHG: snake.rs; ADD: game.rs

--- rsnake/src/main.rs
+++ rsnake/src/main.rs
@@ -2,8 +2,10 @@ use piston_window::*;
 
 mod draw;
 mod snake;
+mod game;
 
 fn main() {
+
     let mut window: PistonWindow =
         WindowSettings::new("Hello World!", [512; 2])
             .build().unwrap();
@@ -11,7 +13,7 @@ fn main() {
         window.draw_2d(&e, |c, g, _| {
             clear([0.5, 0.5, 0.5, 1.0], g);
             rectangle([1.0, 0.0, 0.0, 1.0], // red
-                      [0.0, 0.0, 100.0, 100.0], // rectangle
+                      [10.0, 10.0, 100.0, 100.0], // rectangle
                       c.transform, g);
         });
     }
