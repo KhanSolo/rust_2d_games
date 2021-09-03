197b1acb7e4edd1c7c47d9541ab2a5bb3e68947a
Konstantin <drpdrp@mail.ru>
2021-09-03 23:45:10 +0300

    TECH: game.rs: continue implementation

--- rsnake/src/game.rs
+++ rsnake/src/game.rs
@@ -52,12 +52,48 @@ impl Game {
             Key::Left => Some(Direction::Left),
             Key::Right => Some(Direction::Right),
             _ => None,
-        }
+        };
 
         if dir.unwrap() == self.snake.head_direction().opposite() {
             return
         }
 
+        self.update_snake(dir);
+    }
 
+    pub fn draw(&self, con: &Context, g:&mut G2d){
+        self.snake.draw(con,g);
+
+        if self.food_exists{
+            draw_block(FOOD_COLOR, self.food_x, self.food_y, con, g)
+        }
+
+        draw_rectangle(BORDER_COLOR, 0, 0, self.width, 1, con, g);
+        draw_rectangle(BORDER_COLOR, 0, self.height - 1, self.width, 1, con, g);
+        draw_rectangle(BORDER_COLOR, 0, 0, 1, self.height, con, g);
+        draw_rectangle(BORDER_COLOR, self.width - 1, 0, self.width, 1, con, g);
+
+        if self.game_over {
+            draw_rectangle(GAMEOVER_COLOR, 0, 0, self.width, self.height, con, g);
+        }
+    }
+
+    pub fn update (&mut self, delta_time: f64){
+        self.waiting_time += delta_time;
+
+        if self.game_over {
+            if self.waiting_time > RESTART_TIME {
+                self.restart();
+            }
+            return;
+        }
+
+        if !self.food_exists {
+            self.add_food();
+        }
+
+        if self.waiting_time > MOVING_PERIOD {
+            self.update_snake(None);
+        }
     }
 }
