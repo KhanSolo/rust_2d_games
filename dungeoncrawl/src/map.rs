0cd1b0b3100792c0e75060d6741a231127d8405a
Konstantin <drpdrp@mail.ru>
2021-09-21 19:05:33 +0300

    ADD: State

--- dungeoncrawl/src/map.rs
+++ dungeoncrawl/src/map.rs
@@ -9,30 +9,30 @@ pub enum TileType {
 }
 
 pub struct Map {
-    pub tiles: Vec<TileType>
+    pub tiles: Vec<TileType>,
 }
 
-pub fn map_idx(x: i32, y:i32)-> usize{
-    ((y*SCREEN_WIDTH)+x) as usize
+pub fn map_idx(x: i32, y: i32) -> usize {
+    ((y * SCREEN_WIDTH) + x) as usize
 }
 
-impl Map{
-    pub fn new()->Self{
-        Self{
+impl Map {
+    pub fn new() -> Self {
+        Self {
             tiles: vec![TileType::Floor; NUM_TILES],
         }
     }
 
-    pub fn render(&self, ctx: &mut BTerm){
+    pub fn render(&self, ctx: &mut BTerm) {
         for y in 0..SCREEN_HEIGHT {
-            for x in 0..SCREEN_WIDTH{
-                let idx = map_idx(x,y);
-                match self.tiles[idx]{
+            for x in 0..SCREEN_WIDTH {
+                let idx = map_idx(x, y);
+                match self.tiles[idx] {
                     TileType::Floor => {
-                        ctx.set(x,y, YELLOW, BLACK, to_cp437('.'));
+                        ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
                     }
-                    TileType::Wall =>{
-                        ctx.set(x,y, GREEN, BLACK, to_cp437('#'));
+                    TileType::Wall => {
+                        ctx.set(x, y, GREEN, BLACK, to_cp437('#'));
                     }
                 }
             }
