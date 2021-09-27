fbd8c6b422f457e06fb4a3e8343d60a52fc28a37
Konstantin <drpdrp@mail.ru>
2021-09-27 22:45:03 +0300

    ADD: spawner; spawning player

--- dungeoncrawl/src/main.rs
+++ dungeoncrawl/src/main.rs
@@ -2,6 +2,7 @@ mod camera;
 mod components;
 mod map;
 mod map_builder;
+mod spawner;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
@@ -17,6 +18,7 @@ mod prelude {
     pub use crate::components::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
+    pub use crate::spawner::*;
 }
 
 use prelude::*;
@@ -33,8 +35,10 @@ impl State {
         let mut resources = Resources::default();
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
+
+        spawn_player(&mut ecs, map_builder.player_start);
         resources.insert(map_builder.map);
-        resources.insert(Camera::new(map_builder.player_start))
+        resources.insert(Camera::new(map_builder.player_start));
         Self {
             ecs,
             resources,
